716ce2c86e06e707f2acfa3eaa0fdc1ee62f5953
bananasss00 <bananasss00@yandex.ru>
2021-01-23 22:01:25 +0300

    Init

---
+++ FixDeepStorageHSK/FixDeepStorageHSK.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace FixDeepStorageHSK
+{
+    [StaticConstructorOnStartup]
+    public class FixDeepStorageHSK
+    {
+        static FixDeepStorageHSK()
+        {
+            var h = new Harmony("pirateby.hsk.deepstorage+pickupandhaul.support");
+
+            // get lambda from MakeNewToils => releaseReservation.initAction:
+            //   void PickUpAndHaul.JobDriver_UnloadYourHauledInventory/'<>c__DisplayClass4_0'::'<MakeNewToils>b__3'()
+            var mnt = AccessTools.TypeByName("PickUpAndHaul.JobDriver_UnloadYourHauledInventory")?
+                .GetNestedTypes(BindingFlags.Instance | BindingFlags.NonPublic)
+                .First(t => t.FullName.Contains("c__DisplayClass4_0"))?
+                .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                .First(x => x.Name.EndsWith("b__3") && x.ReturnType == typeof(void));
+
+            if (mnt != null)
+            {
+                
+                h.Patch(mnt, transpiler: new HarmonyMethod(typeof(FixDeepStorageHSK), nameof(Transpiler)));
+                Log.Message($"[FixDeepStorageHSK] PickUpAndHaul fixed for comp with LWM.DeepStorage");
+            }
+
+            var rocketman = AccessTools.TypeByName("RocketMan.Optimizations.StatWorker_GetValueUnfinalized_Hijacked_Patch");
+            if (rocketman != null)
+            {
+                var replacemant = AccessTools.Method(rocketman, "Replacemant");
+                //var expireCache = AccessTools.Method(rocketman, "ProcessExpiryCache");
+                //var updateCache = AccessTools.Method(rocketman, "UpdateCache");
+                if (replacemant != null)
+                {
+                    h.Patch(replacemant, prefix: new HarmonyMethod(typeof(FixDeepStorageHSK), nameof(RocketMan_Replacemant)));
+                    Log.Message($"[FixDeepStorageHSK] RocketMan2 fixed");
+                }
+            }
+
+            ListerHaulablesThreaded.InitPatches(h);
+        }
+
+        public static bool RocketMan_Replacemant(StatWorker statWorker, StatRequest req, bool applyPostProcess, ref float __result)
+        {
+            if (!UnityData.IsInMainThread)
+            {
+				// return not cached
+                __result = statWorker.GetValueUnfinalized(req, applyPostProcess);
+                return false;
+            }
+
+            if (req.thingInt == null && req.stuffDefInt == null && req.defInt == null)
+            {
+                Log.ErrorOnce($"Try get value from Empty StatRequest!", "rocketman.patch".GetHashCode());
+                __result = 0;
+                return false;
+            }
+            return true;
+        }
+
+        private static bool RunIfMainThread() => UnityData.IsInMainThread;
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var hskActive = AccessTools.Method("PickUpAndHaul.ModCompatibilityCheck:get_HCSKIsActive");
+            if (hskActive == null)
+            {
+                Log.Error($"[FixDeepStorageHSK] PickUpAndHaul.ModCompatibilityCheck:get_HCSKIsActive not found.");
+                yield break;
+            }
+
+            /*
+            18	0047	call	bool PickUpAndHaul.ModCompatibilityCheck::get_HCSKIsActive()
+            19	004C	brtrue.s	37 (0093) ret 
+             */
+            bool skipNext = false;
+            foreach (var ci in instructions)
+            {
+                if (ci.opcode == OpCodes.Call && ci.operand == hskActive) skipNext = true;
+                else if (skipNext) skipNext = false;
+                else yield return ci;
+            }
+
+            /*
+            Toil releaseReservation = new Toil
+            {
+                initAction = () =>
+                {
+                    if (pawn.Map.reservationManager.ReservedBy(job.targetB, pawn, pawn.CurJob)
+                        // REMOVED THIS CHECK => && !ModCompatibilityCheck.HCSKIsActive
+                    )
+                        pawn.Map.reservationManager.Release(job.targetB, pawn, pawn.CurJob);
+                }
+            };
+             */
+        }
+    }
+}
---
+++ FixDeepStorageHSK/ListerHaulablesThreaded.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace FixDeepStorageHSK
+{
+    public static class ListerHaulablesThreaded
+    {
+        private const int PatchPriority = Priority.First;
+        private const int ThreadUpdateInterval = 60;
+        private static Thread _thread;
+        private static readonly object Locker = new object();
+
+        public static void InitPatches(Harmony h)
+        {
+            h.Patch(AccessTools.Method(typeof(ListerHaulables), nameof(ListerHaulables.ListerHaulablesTick)), prefix: new HarmonyMethod(typeof(ListerHaulablesThreaded), nameof(ListerHaulablesTick_Disabled)) {priority = PatchPriority});
+            h.Patch(AccessTools.Method(typeof(ListerHaulables), nameof(ListerHaulables.ThingsPotentiallyNeedingHauling)), prefix: new HarmonyMethod(typeof(ListerHaulablesThreaded), nameof(ThingsPotentiallyNeedingHauling)) {priority = PatchPriority});
+            h.Patch(AccessTools.Method(typeof(ListerHaulables), nameof(ListerHaulables.Check)), prefix: new HarmonyMethod(typeof(ListerHaulablesThreaded), nameof(Check)) {priority = PatchPriority});
+            h.Patch(AccessTools.Method(typeof(ListerHaulables), nameof(ListerHaulables.CheckAdd)), prefix: new HarmonyMethod(typeof(ListerHaulablesThreaded), nameof(CheckAdd)) {priority = PatchPriority});
+            h.Patch(AccessTools.Method(typeof(ListerHaulables), nameof(ListerHaulables.TryRemove)), prefix: new HarmonyMethod(typeof(ListerHaulablesThreaded), nameof(TryRemove)) {priority = PatchPriority});
+
+            _thread = new Thread(() =>
+            {
+                Log.Message($"[FixDeepStorageHSK] ListerHaulablesTick thread started");
+                while (true)
+                {
+                    try
+                    {
+                        var maps = Find.Maps;
+                        if (maps != null)
+                            foreach (var map in maps)
+                                map.listerHaulables.ListerHaulablesTick_New();
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error($"[FixDeepStorageHSK] exception in thread: {e}");
+                    }
+                    Thread.Sleep(ThreadUpdateInterval);
+                }
+            });
+            _thread.Start();
+        }
+
+        // patch: disable original function
+        private static bool ListerHaulablesTick_Disabled() => false;
+
+        // internal check without lock object
+        private static void Check_Internal(ListerHaulables lister, Thing t)
+        {
+            if (lister.ShouldBeHaulable(t))
+            {
+                if (!lister.haulables.Contains(t))
+                {
+                    lister.haulables.Add(t);
+                }
+            }
+            else if (lister.haulables.Contains(t))
+            {
+                lister.haulables.Remove(t);
+            }
+        }
+
+        // patch: ListerHaulablesTick replacement in thread
+        private static void ListerHaulablesTick_New(this ListerHaulables lister)
+        {
+            ListerHaulables.groupCycleIndex++;
+
+            if (ListerHaulables.groupCycleIndex >= int.MaxValue)
+                ListerHaulables.groupCycleIndex = 0;
+
+            var allGroupsListForReading = lister.map.haulDestinationManager.AllGroupsListForReading;
+            if (allGroupsListForReading.Count == 0)
+                return;
+
+            var index = ListerHaulables.groupCycleIndex % allGroupsListForReading.Count;
+            var slotGroup = allGroupsListForReading[ListerHaulables.groupCycleIndex % allGroupsListForReading.Count];
+            if (slotGroup.CellsList.Count != 0)
+            {
+                while (lister.cellCycleIndices.Count <= index)
+                    lister.cellCycleIndices.Add(0);
+                if (lister.cellCycleIndices[index] >= int.MaxValue)
+                    lister.cellCycleIndices[index] = 0;
+
+                for (var i = 0; i < 4; i++)
+                {
+                    var list = lister.cellCycleIndices;
+                    list[index]++;
+                    var thingList = slotGroup.CellsList[lister.cellCycleIndices[index] % slotGroup.CellsList.Count].GetThingList(lister.map)
+                        .ToList(); // Make copy of the list. Prevent System.InvalidOperationException: Collection was modified; enumeration operation may not execute.
+                    foreach (var thing in thingList)
+                    {
+                        if (thing.def.EverHaulable)
+                        {
+                            // this.Check(thingList[j]);
+                            Check_Internal(lister, thing);
+                            /* LWM.DeepStorage remove this break in transpiler */
+                            // break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool ThingsPotentiallyNeedingHauling(ListerHaulables __instance, ref List<Thing> __result)
+        {
+            lock (Locker)
+            {
+                __result = new List<Thing>(__instance.haulables);
+                //__result = __instance.haulables;
+            }
+            return false;
+        }
+
+        private static bool Check(ListerHaulables __instance, Thing t)
+        {
+            lock (Locker)
+            {
+                Check_Internal(__instance, t);
+            }
+            return false;
+        }
+	
+        private static bool CheckAdd(ListerHaulables __instance, Thing t)
+        {
+            lock (Locker)
+            {
+                if (__instance.ShouldBeHaulable(t) && !__instance.haulables.Contains(t))
+                {
+                    __instance.haulables.Add(t);
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TryRemove(ListerHaulables __instance, Thing t)
+        {
+            lock (Locker)
+            {
+                if (t.def.category == ThingCategory.Item && __instance.haulables.Contains(t))
+                {
+                    __instance.haulables.Remove(t);
+                }
+            }
+            return false;
+        }
+    }
+}
